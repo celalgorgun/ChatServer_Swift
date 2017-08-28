31d7f5cff3f7c3b8192009ca896614ea2d723e88
Unknown <sambrose@nebrwesleyan.edu>
2017-08-28 18:11:12 -0500

    api_dev-1: Fixed build linker error and removed depens for now

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-08-28 18:48:27 +0000
+// Date: 2017-08-28 22:54:14 +0000
 import PackageDescription
 let package = Package(
 	name: "Perfect-JSON-API",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
 	]
 )
